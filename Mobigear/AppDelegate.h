43fea333116b51b0961ef15901747ae6a67079e0
kilovata <kilovata@gmail.com>
2015-04-06 19:35:48 +0300

    Initial Commit

---
+++ Mobigear/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Mobigear
+//
+//  Created by Sveta Kilovata on 06/04/15.
+//  Copyright (c) 2015 kilovata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Mobigear/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mobigear
+//
+//  Created by Sveta Kilovata on 06/04/15.
+//  Copyright (c) 2015 kilovata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
